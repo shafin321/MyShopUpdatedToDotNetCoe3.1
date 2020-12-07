b90433d16ee0be6d4132e50c965ca6baafcf38e1
shafin321 <shafinhaque91@gmail.com>
2020-12-07 02:14:23 +0100

    Implemented InMemoryCategoryRepo

---
+++ MyShop.DataAccess.InMemory/InMemoryCategoryRepo.cs
@@ -0,0 +1,78 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    class InMemoryCategoryRepo
+    {
+        ObjectCache cache = MemoryCache.Default;
+
+        List<ProductCategory> productCategories;
+
+        public InMemoryCategoryRepo()
+        {
+            productCategories = cache["productCategories"] as List<ProductCategory>;
+
+            if (productCategories == null)
+            {
+                productCategories = new List<ProductCategory>();
+            }
+
+        }
+
+        public void Commit()
+        {
+            cache["productCategories"] = productCategories; // save on cache
+        }
+
+        public void Insert(ProductCategory category)
+        {
+            productCategories.Add(category);
+        }
+
+        public void Update(ProductCategory productCategory )
+        {
+            var productToUpdate = productCategories.FirstOrDefault(c => c.Id == productCategory.Id);
+             
+            if(productToUpdate != null)
+            {
+                //updateing product->incoming product->productCategory
+                productToUpdate = productCategory;
+            }
+
+            else
+            {
+                throw new Exception("Not found");
+            }
+        }
+
+        public IEnumerable<ProductCategory> GetAll()
+        {
+            return productCategories;
+        }
+
+        public ProductCategory GetById(string id)
+        {
+            return productCategories.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Delete(string id)
+        {
+            var productToDelete = productCategories.FirstOrDefault(c => c.Id == id);
+            if(productCategories != null)
+            {
+                productCategories.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("Not found");
+            }
+           
+        }
+    }
+}
---
+++ MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyShop.Core.Contracts;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+        private IMemoryProductRepo _categoryRepo;
+
+        public ProductCategoryManagerController(IMemoryProductRepo categoryRepo)
+        {
+            _categoryRepo = categoryRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
